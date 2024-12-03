c3f7df7e6de10e39bec0940073794f328ad6d3fc
APKonovalov <APKonovalov@alfabank.ru>
2024-12-03 23:36:49 +0300

    update appium version

--- lib/src/main/java/ru/otus/qa/appium/Main.java
+++ lib/src/main/java/ru/otus/qa/appium/Main.java
@@ -2,11 +2,9 @@ package ru.otus.qa.appium;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.remote.AutomationName;
-import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Paths;
@@ -14,14 +12,12 @@ import java.nio.file.Paths;
 public class Main {
     public static void main(String[] args) {
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID);
-        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, AutomationName.ANDROID_UIAUTOMATOR2);
-        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "emulator-5554");
-        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "emulator-5554");
-        capabilities.setCapability(MobileCapabilityType.APP, Paths.get("skoda.apk").toAbsolutePath().toString());
-        capabilities.setCapability("autoGrantPermissions", true);
+        capabilities.setCapability("platformName", Platform.ANDROID);
+        capabilities.setCapability("appium:automationName", AutomationName.ANDROID_UIAUTOMATOR2);
+        capabilities.setCapability("appium:deviceName", "emulator-5554");
+        capabilities.setCapability("appium:app", Paths.get("skoda.apk").toAbsolutePath().toString());
         try {
-            AppiumDriver appiumDriver = new AppiumDriver(new URL("http://0.0.0.0:47231/wd/hub"), capabilities);
+            AppiumDriver appiumDriver = new AppiumDriver(new URL("http://0.0.0.0:4723/wd/hub"), capabilities);
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         }
